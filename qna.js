ed58b7a16247abdf306b40e1fcfd9004e710397f
akt1108 <96526690+akt1108@users.noreply.github.com>
2022-05-13 03:25:16 +0900

    version_0.5

---
+++ qna.js
@@ -0,0 +1,125 @@
+            var word=text.split(/\r\n|\r|\n/);
+            var question=[], a=[], b=[], c=[], d=[];
+            for(i=0; i<word.length; i++){
+                if(i%5===0){
+                    question.push(word[i]);
+                }else if(i%5===1){
+                    a.push(word[i])
+                }else if(i%5===2){
+                    b.push(word[i])
+                }else if(i%5===3){
+                    c.push(word[i])
+                }else if(i%5===4){
+                    d.push(word[i])
+                }
+            }
+
+            function displayQuestion(num){
+                document.write("<div class=\"unit\"><div class=\"question\">"+question[num]+"</div><div id=\"a"+num+"\">"+a[num]+"</div><div id=\"b"+num+"\">"+b[num]+"</div><div id=\"c"+num+"\">"+c[num]+"</div><div id=\"d"+num+"\">"+d[num]+"</div></div>");
+            }
+
+            function addAnswerClass(num){
+                if (answer[num]=="a"){
+                    document.getElementById("a"+num).className += "answer"
+                }else if(answer[num]=="b"){
+                    document.getElementById("b"+num).className += "answer"
+                }else if(answer[num]=="c"){
+                    document.getElementById("c"+num).className += "answer"
+                }else if(answer[num]=="d"){
+                    document.getElementById("d"+num).className += "answer"
+                }
+            }
+
+            function getRandomInt(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+            
+            function marking(num){
+                document.getElementById("a"+num).addEventListener("click", markingA);
+                document.getElementById("b"+num).addEventListener("click", markingB);
+                document.getElementById("c"+num).addEventListener("click", markingC);
+                document.getElementById("d"+num).addEventListener("click", markingD);
+                document.getElementById("a"+num).style.marginTop="3rem";
+                document.getElementById("b"+num).style.marginTop="3rem";
+                document.getElementById("c"+num).style.marginTop="3rem";
+                document.getElementById("d"+num).style.marginTop="3rem";
+                document.getElementById("a"+num).style.marginBottom="3rem";
+                document.getElementById("b"+num).style.marginBottom="3rem";
+                document.getElementById("c"+num).style.marginBottom="3rem";
+                document.getElementById("d"+num).style.marginBottom="3rem";
+            }
+
+            function markingA(){
+                if (answer[num]=="a"){
+                    document.getElementById("a"+num).className += "correct"
+                }else if(answer[num]=="b"){
+                    document.getElementById("a"+num).className += "wrong"
+                    document.getElementById("b"+num).className += "answer"
+                }else if(answer[num]=="c"){
+                    document.getElementById("a"+num).className += "wrong"
+                    document.getElementById("c"+num).className += "answer"
+                }else if(answer[num]=="d"){
+                    document.getElementById("a"+num).className += "wrong"
+                    document.getElementById("d"+num).className += "answer"
+                }
+                document.getElementById("a"+num).removeEventListener("click", markingA);
+                document.getElementById("b"+num).removeEventListener("click", markingB);
+                document.getElementById("c"+num).removeEventListener("click", markingC);
+                document.getElementById("d"+num).removeEventListener("click", markingD);
+            }
+            function markingB(){
+                if (answer[num]=="a"){
+                    document.getElementById("b"+num).className += "wrong"
+                    document.getElementById("a"+num).className += "answer"
+                }else if(answer[num]=="b"){
+                    document.getElementById("b"+num).className += "correct"
+                }else if(answer[num]=="c"){
+                    document.getElementById("b"+num).className += "wrong"
+                    document.getElementById("c"+num).className += "answer"
+                }else if(answer[num]=="d"){
+                    document.getElementById("b"+num).className += "wrong"
+                    document.getElementById("d"+num).className += "answer"
+                }
+                document.getElementById("a"+num).removeEventListener("click", markingA);
+                document.getElementById("b"+num).removeEventListener("click", markingB);
+                document.getElementById("c"+num).removeEventListener("click", markingC);
+                document.getElementById("d"+num).removeEventListener("click", markingD);
+            }
+            function markingC(){
+                if (answer[num]=="a"){
+                    document.getElementById("c"+num).className += "wrong"
+                    document.getElementById("a"+num).className += "answer"
+                }else if(answer[num]=="b"){
+                    document.getElementById("c"+num).className += "wrong"
+                    document.getElementById("b"+num).className += "answer"
+                }else if(answer[num]=="c"){
+                    document.getElementById("c"+num).className += "correct"
+                }else if(answer[num]=="d"){
+                    document.getElementById("c"+num).className += "wrong"
+                    document.getElementById("d"+num).className += "answer"
+                }
+                document.getElementById("a"+num).removeEventListener("click", markingA);
+                document.getElementById("b"+num).removeEventListener("click", markingB);
+                document.getElementById("c"+num).removeEventListener("click", markingC);
+                document.getElementById("d"+num).removeEventListener("click", markingD);
+            }
+            function markingD(){
+                if (answer[num]=="a"){
+                    document.getElementById("d"+num).className += "wrong"
+                    document.getElementById("a"+num).className += "answer"
+                }else if(answer[num]=="b"){
+                    document.getElementById("d"+num).className += "wrong"
+                    document.getElementById("b"+num).className += "answer"
+                }else if(answer[num]=="c"){
+                    document.getElementById("d"+num).className += "wrong"
+                    document.getElementById("c"+num).className += "answer"
+                }else if(answer[num]=="d"){
+                    document.getElementById("d"+num).className += "correct"
+                }
+                document.getElementById("a"+num).removeEventListener("click", markingA);
+                document.getElementById("b"+num).removeEventListener("click", markingB);
+                document.getElementById("c"+num).removeEventListener("click", markingC);
+                document.getElementById("d"+num).removeEventListener("click", markingD);
+            }
---
+++ qna7.js
@@ -0,0 +1,177 @@
+            var text=
+            "1. Which of the following is the first phase of a typical product development process?\n"+
+            "a. Product/process development\n"+
+            "b. Idea generation\n"+
+            "c. Preliminary concept development\n"+
+            "d. Market introduction\n"+
+            "2. New ideas are studied for feasibility in the _____ phase of product development.\n"+
+            "a. market evaluation\n"+
+            "b. process development\n"+
+            "c. full-scale production \n"+
+            "d. preliminary concept development\n"+
+            "3. Which of the following phases of product development usually includes prototype testing?\n"+
+            "a. Product/process development\n"+
+            "b. Idea generation\n"+
+            "c. Full-scale production\n"+
+            "d. Market evaluation\n"+
+            "4. The product is distributed to customers in the _____ phase of product development.\n"+
+            "a. preliminary concept development\n"+
+            "b. market introduction\n"+
+            "c. idea generation\n"+
+            "d. full-scale production\n"+
+            "5. _____ is a process in which all major functions involved with bringing a product to market are continuously involved with product development from conception through sales.\n"+
+            "a. Six sigma\n"+
+            "b. Concurrent engineering\n"+
+            "c. Organizational learning\n"+
+            "d. Axiomatic design\n"+
+            "6. Which of the following activities of the design for six sigma (DFSS) focuses on creating and building a product idea and determining its functionality based upon customer requirements, technological capabilities, and economic realities?\n"+
+            "a. Detailed design\n"+
+            "b. Design verification\n"+
+            "c. Concept development\n"+
+            "d. Design optimization\n"+
+            "7. Which of the following activities of the design for six sigma (DFSS) focuses on developing specific requirements and design parameters such as specifications and tolerances to ensure that the product fulfills the functional requirements of the concept?\n"+
+            "a. Concept development\n"+
+            "b. Design verification\n"+
+            "c. Design optimization\n"+
+            "d. Detailed design\n"+
+            "8. Which of the following activities of the design for six sigma (DFSS) seeks to refine designs to identify and eliminate potential failures, achieve high reliability, and ensure that it can be easily manufactured, assembled, or delivered in an environmentally-responsible manner?\n"+
+            "a. Detailed design\n"+
+            "b. Design optimization\n"+
+            "c. Concept development\n"+
+            "d. Design verification\n"+
+            "9. Which of the following activities of the design for six sigma (DFSS) ensures that the quality level and reliability requirements of the product are achieved?\n"+
+            "a. Design verification\n"+
+            "b. Design optimization\n"+
+            "c. Detailed design\n"+
+            "d. Concept development\n"+
+            "10. The DMADV process incorporates the principal activities of the design for six sigma. Which of the following activities included in DMADV focuses on identifying and understanding the market need or opportunity?\n"+
+            "a. Design\n"+
+            "b. Measure\n"+
+            "c. Analyze\n"+
+            "d. Define\n"+
+            "11. Which of the following activities included in the DMADV process gathers the voice of the customer, identifies the vital characteristics that are most important to customers, and outlines the functional requirements of the product that will meet customer needs?\n"+
+            "a. Design\n"+
+            "b. Measure\n"+
+            "c. Analyze\n"+
+            "d. Verify\n"+
+            "12. Which of the following activities included in the DMADV process is focused on concept development from engineering and aesthetic perspectives and often includes the creation of drawings, virtual models, or simulations to develop and understand the functional characteristic of the product?\n"+
+            "a. Measure\n"+
+            "b. Define\n"+
+            "c. Verify\n"+
+            "d. Analyze\n"+
+            "13. Which of the following activities included in the DMADV process focuses on developing detailed specifications, purchasing requirements, and so on, so that the concept can be produced?\n"+
+            "a. Measure\n"+
+            "b. Define\n"+
+            "c. Design\n"+
+            "d. Analyze\n"+
+            "14. Which of the following activities involved in the DMADV process involves prototype development, testing, and implementation planning for production?\n"+
+            "a. Define\n"+
+            "b. Measure\n"+
+            "c. Design\n"+
+            "d. Verify\n"+
+            "15. _____ involves the adoption of an idea, process, technology, product, or business model that is either new or new to its proposed application.\n"+
+            "a. Concurrent engineering\n"+
+            "b. Total quality management (TQM)\n"+
+            "c. Design optimization\n"+
+            "d. Innovation\n"+
+            "16. _____ is seeing things in new or novel ways.\n"+
+            "a. Flexibility\n"+
+            "b. Agility\n"+
+            "c. Creativity\n"+
+            "d. Quality assurance\n"+
+            "17. Which of the following is a creativity tool that finds extensive use in product design?\n"+
+            "a. TRIZ\n"+
+            "b. Fault tree analysis\n"+
+            "c. Likert scale\n"+
+            "d. Cause-and-effect diagram\n"+
+            "18. _____ is a methodology used in the product design process, developed by Dr. Nam Suh from MIT, which is based on the premise that good design is governed by laws similar to those in natural science.\n"+
+            "a. Fault tree analysis\n"+
+            "b. Axiomatic design\n"+
+            "c. Design failure mode and effects analysis (DFMEA)\n"+
+            "d. Six sigma\n"+
+            "19. In the context of the axiomatic design method, the information axiom states that: \n"+
+            "a. good design occurs when the functional requirements of the design are independent of one another.\n"+
+            "b. developing specific requirements and design parameters such as specifications and tolerances leads to the creation of a high quality product.\n"+
+            "c. good design corresponds to minimum complexity.\n"+
+            "d. all the departments in an organization should be continuously involved with product development from conception through sales.\n"+
+            "20. In the context of the axiomatic design method, the independence axiom states that: \n"+
+            "a. good design occurs when the functional requirements of the design are separate from one another.\n"+
+            "b. developing specific requirements and design parameters such as specifications and tolerances leads to the creation of a high quality product.\n"+
+            "c. good design corresponds to minimum complexity.\n"+
+            "d. all the departments in an organization should be continuously involved with product development from conception through sales.\n"+
+            "21. Which of the following tools is most likely to be used for establishing technical design requirements that meet customer needs and placing them in subsequent production activities?\n"+
+            "a. Likert scale\n"+
+            "b. Quality function deployment\n"+
+            "c. Cause-and-effect diagram\n"+
+            "d. Fault tree analysis\n"+
+            "22. The matrix used by the quality function deployment to ensure that the voice of the customer is carried throughout the production/delivery process is called the _____.\n"+
+            "a. Hidden Factory\n"+
+            "b. Quincunx\n"+
+            "c. Likert Scale\n"+
+            "d. House of Quality\n"+
+            "23. The first House of Quality relates:\n"+
+            "a. component requirements to process operations.\n"+
+            "b. customer requirements to a product’s overall technical requirements.\n"+
+            "c. process operations to quality control plans\n"+
+            "d. technical requirements of a product to component requirements.\n"+
+            "24. The final House of Quality relates:\n"+
+            "a. process operations to component requirements.\n"+
+            "b. customer requirements to technical requirements.\n"+
+            "c. technical requirements to component requirements.\n"+
+            "d. process operations to quality control plans.\n"+
+            "25. Which of the following is the first House of Quality matrix?\n"+
+            "a. Customer requirement planning matrix\n"+
+            "b. Process operations matrix\n"+
+            "c. Technical requirement planning matrix\n"+
+            "d. Component characteristics matrix\n"+
+            "26. Which of the following symbols is used to denote a very strong relationship between any pair of technical requirements in a typical House of Quality?\n"+
+            "a. *\n"+
+            "b. Δ\n"+
+            "c. ^\n"+
+            "d.  •\n"+
+            "27. Which of the following symbols is used to denote a weak relationship between any pair of technical requirements in a typical House of Quality?\n"+
+            "a.  •\n"+
+            "b. *\n"+
+            "c. Δ\n"+
+            "d. #\n"+
+            "28. In the context of manufacturing specifications, the term “tolerance” refers to:\n"+
+            "a. the target value that manufacturing seeks to meet.\n"+
+            "b. the specification that best represents the requirements of the customer.\n"+
+            "c. the specification which leads to the development of the least costly product.\n"+
+            "d. the permissible variation, recognizing the difficulty of meeting a target consistently.\n"+
+            "29. The predicted reliability determined by the design of the product or process is called the _____ reliability.\n"+
+            "a. achieved \n"+
+            "b. perceived \n"+
+            "c. inherent \n"+
+            "d. nominal \n"+
+            "30. A personal computer system is composed of the processing unit, graphics board, and keyboard with reliabilities of 0.976, 0.785, and 0.893, respectively. This is a series system and even if one component fails, the computer will not function correctly. The reliability of this system is:\n"+
+            "a. 0.684\n"+
+            "b. 0.953\n"+
+            "c. 2.654\n"+
+            "d. 0.857\n"+
+            "31. An electric component has a failure rate of λ= 0.0035 failure per hour. For this component, the mean time to failure(θ) is _____.\n"+
+            "a. 356.8 hours\n"+
+            "b. 285.7 hours\n"+
+            "c. 532.7 hours\n"+
+            "d. 258 hours\n"+
+            "32. Robust design refers to designing goods and services that:\n"+
+            "a. make the most efficient use of the available resources.\n"+
+            "b. create a competitive advantage for the organization in the market.\n"+
+            "c. are insensitive to variation in manufacturing processes and when consumers use them.\n"+
+            "d. are the least costly to produce.\n"+
+            "33. _____ is a method to describe combinations of conditions or events that can lead to a failure.\n"+
+            "a. Axiomatic design\n"+
+            "b. Quality function deployment\n"+
+            "c. Fault tree analysis\n"+
+            "d. Total quality management\n"+
+            "34. _____ is the process of designing a product for efficient production at the highest level of quality.\n"+
+            "a. Design for manufacturability\n"+
+            "b. Concurrent engineering\n"+
+            "c. Design for disassembly \n"+
+            "d. Design for environment\n"+
+            "35. Accelerated life testing involves:\n"+
+            "a. using the fault tree analysis technique to find the defects in the product.\n"+
+            "b. using the TRIZ tool to identify faults in the designing of the product.\n"+
+            "c. overstressing components to reduce the time to failure and find weaknesses.\n"+
+            "d. finding the degree to which the product meets the customer requirements."
+            var answer = ["b", "d", "a", "b", "b", "c", "d", "b", "a", "d", "b", "d", "c", "d", "d", "c", "a", "b", "c", "a", "b", "d", "b", "d", "a", "d", "c", "d", "c", "a", "b", "c", "c", "a", "c"]
